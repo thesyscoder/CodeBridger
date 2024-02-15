a53ba29ac8f2deda807652ed2fc03aee76b84b3e
thesyscoder <pruthvirajs2007@outlook.com>
2024-02-15 12:12:12 +0530

    Array Practice[easy] - find average of array elements-updated

--- DSA/data_structure/array/practice/easy/find_average_array.js
+++ DSA/data_structure/array/practice/easy/find_average_array.js
@@ -1,13 +1,26 @@
+/**
+ * Calculates the sum and average of elements in the given array.
+ *
+ * @param {number[]} arr - The input array of numbers.
+ * @returns {Object} - An object containing two properties: 'sum' for the sum of elements and 'avg' for the average of elements in the array.
+ * @description This function calculates the sum and average of elements in the input array.
+ * Time Complexity: O(n) where n is the number of elements in the array.
+ * Space Complexity: O(1) - The space used is constant regardless of the size of the input array.
+ */
 function findAverage(arr) {
   let sum = 0;
   let avg = 0;
+
   if (arr.length === 0) {
-    return undefined;
+    return undefined; // or handle the empty array case based on your requirements
   }
-  for (let i = 0; i <= arr.length - 1; i++) {
+
+  for (let i = 0; i < arr.length; i++) {
     sum += arr[i];
   }
+
   avg = sum / arr.length;
+
   return { sum, avg };
 }
 

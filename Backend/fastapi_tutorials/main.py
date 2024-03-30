067ff851f26462980b6a36852462cf0a5b0fe3f3
Pruthviraj Sonawane <103118127+thesyscoder@users.noreply.github.com>
2024-03-30 13:01:36 +0000

    Fats API: Status code added

--- Backend/fastapi_tutorials/main.py
+++ Backend/fastapi_tutorials/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Query, status, Response
 from enum import Enum
 
 app = FastAPI()
@@ -21,13 +21,17 @@ def index():
 # Path parameter
 
 
-@app.get("/blogs/{id}")
-def get_blog_by_id(id: int):
+@app.get("/blogs/{id}", status_code=status.HTTP_200_OK)
+def get_blog_by_id(id: int, response: Response):
     """
     Retrieve a blog by its ID.
     """
-    return {"message": f"Blog by {id}"}
-
+    if id == 1:
+        response.status_code = status.HTTP_200_OK
+        return {"message": "Blog id {id}"}
+    else:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"message": "Blog not found"}
 # Path parameters - Type parameters
 
 

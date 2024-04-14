ef388584575b9278b8fc179102c9d2d1b65f10e6
thesyscoder <pruthvirajs2007@outlook.com>
2024-04-14 17:34:45 +0530

    deleted backend projects

---
+++ Database/postgress/queries/arithmetic_operators.sql
@@ -0,0 +1,38 @@
+-- Title: Arithmetic Operators
+-- Description: This script demonstrates the usage of arithmetic operators in SQL queries.
+-- Author: thesyscoder
+--
+-- Syntax:
+--     SELECT expression AS result;
+--
+-- Example:
+--     -- Perform addition
+--     SELECT 10 + 12 AS sum;
+--     -- Output: Display the sum of 10 and 12.
+--
+--     -- Perform subtraction
+--     SELECT 20 - 4 AS Sub;
+--     -- Output: Display the subtraction of 4 from 20.
+--
+--     -- Perform multiplication
+--     SELECT 20 * 3 + 10 AS Mul;
+--     -- Output: Display the result of multiplying 20 by 3 and then adding 10.
+--
+--     -- Perform division
+--     SELECT 30 / 10 AS div;
+--     -- Output: Display the result of dividing 30 by 10.
+-- Perform addition
+SELECT
+    10 + 12 AS sum;
+
+-- Perform subtraction
+SELECT
+    20 - 4 AS Sub;
+
+-- Perform multiplication
+SELECT
+    20 * 3 + 10 AS Mul;
+
+-- Perform division
+SELECT
+    30 / 10 AS div;

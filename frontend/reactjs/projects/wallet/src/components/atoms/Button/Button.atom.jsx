8c5909dbc766a30d32fd4baac732f79a5aabb232
thesyscoder <pruthvirajs2007@outlook.com>
2024-04-29 00:35:34 +0530

    linking done

--- frontend/reactjs/projects/wallet/src/components/atoms/Button/Button.atom.jsx
+++ frontend/reactjs/projects/wallet/src/components/atoms/Button/Button.atom.jsx
@@ -3,10 +3,12 @@
 import React from "react";
 import "./button.style.css";
 
-const Button = ({ className = "primary", title }) => {
+const Button = ({ className = "primary", title, onClickAction }) => {
   return (
     <div className="button-container">
-      <button className={className}>{title}</button>
+      <button className={className} onClick={onClickAction}>
+        {title}
+      </button>
     </div>
   );
 };
